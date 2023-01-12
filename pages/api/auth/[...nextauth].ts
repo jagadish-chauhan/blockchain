338550477984734ac436c08f6301ed16ac69f71c
Jagadish Chauhan <myjagdish@yahoo.com>
2023-01-12 23:01:40 +0530

    Add mongodb connection

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
 import NextAuth from 'next-auth';
 import { MoralisNextAuthProvider } from '@moralisweb3/next';
-
+import connectDB from '../../../lib/connectDB';
 export default NextAuth({
     providers: [MoralisNextAuthProvider()],
     // adding user info to the user session object
     callbacks: {
         async jwt({ token, user }) {
+            await connectDB();
+
             if (user) {
                 token.user = user;
             }
