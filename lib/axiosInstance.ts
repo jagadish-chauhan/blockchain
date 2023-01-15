7a360ec166b0dc570380fb765cfb781561bc7dc0
Jagadish Chauhan <myjagdish@yahoo.com>
2023-01-15 20:08:09 +0530

    change env

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXTAUTH_URL ?? 'http://localhost:3000',
+  baseURL: process.env.NEXTAUTH_URL,
   // timeout: 1000,
   // headers: {
   //   'content-type': 'application/json',
